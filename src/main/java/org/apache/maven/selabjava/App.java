a04a809ecd9f783879dd6cc2eb66461222455e17
Indira <indira.pagadala28@gmail.com>
2024-12-31 10:55:24 +0530

    Initial commit

---
+++ src/main/java/org/apache/maven/selabjava/App.java
@@ -0,0 +1,13 @@
+package org.apache.maven.selabjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SE LAB 2!" );
+    }
+}
